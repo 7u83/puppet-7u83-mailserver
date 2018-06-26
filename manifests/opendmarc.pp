0cf32071ad9a41242257016e751b70bc688ac1f3
7u83 <7u83@mail.ru>
2018-06-26 22:58:31 +0000

    opendrmarc files

---
+++ manifests/opendmarc.pp
@@ -0,0 +1,49 @@
+#
+# install opendmarc
+#
+
+class mailserver::opendmarc(
+	$umask = undef,
+	$reject_failures = undef,
+	$software_header = undef,
+	$spf_self_validate,
+) 
+{
+        case $::osfamily {
+                'FreeBSD':{
+			$conf = "/usr/local/etc/mail/opendmarc.conf"
+			$pkg = "opendmarc"
+			$service = "opendmarc"
+			$milter_socket = "/var/run/opendmarc/milter"
+
+			package { "$pkg":
+				ensure => installed,
+				require => Mailserver::Sysrc["opendmarc_socketspec"]
+			}
+
+			mailserver::sysrc{"opendmarc_socketspec":
+				ensure => "$milter_socket"
+			}
+
+		}
+		default: {
+			package { "opendmarc":
+				ensure => installed
+			}
+
+		}
+	}
+	
+	service{ "$service":
+		ensure => running,
+		require => File["$conf"],
+		subscribe => File["$conf"],
+	}
+
+	file { "$conf":
+		ensure => present,
+		content => template("mailserver/opendmarc.conf.erb"),
+		require => Package["$pkg"],
+	}
+
+}
