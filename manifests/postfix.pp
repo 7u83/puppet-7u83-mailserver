eb880434a4c6d293d90caa25af18520544d57f70
7u83 <7u83@mail.ru>
2019-12-01 14:27:06 +0000

    Installs postfix on Debian or FreeBSD
    Also postgresql support is included

--- manifests/postfix.pp
+++ manifests/postfix.pp
@@ -1,52 +1,51 @@
 #postfix
 #
 class mailserver::postfix::params(){
+
 }
 
 
 
 class mailserver::postfix(
-	$ldap = true,
+	$ldap = false,
 	$mysql = false,
+	$pgsql = false,
 
 ) inherits ::mailserver::postfix::params{
+
         case $::osfamily {
                 'FreeBSD':{
+			$packages = 'postfix'
 
-			if $ldap {
-				package {"postfix":
-					provider => "portsng",
-					ensure => 'installed',
-					package_settings => {
-						'LDAP' => $ldap,
-						'MYSQL' => $mysql,
-					},
-					require => Package["portupgrade"]
+			if $ldap or $mysql {
+				$package_settings = {
+					'LDAP' => $ldap,
+					'MYSQL' => $mysql,
+					'PGSQL' => $pgsql,
 				}
-			}
-			else {
-				package {"postfix":
-					ensure => 'installed'
-				}
+				$package_provider = "portsng"
+				$package_reqquire = Package["portsng"]
 			}
 
-#			ensure_resource("file","/usr/local/etc/mail",{
-#				ensure => directory
-#			})
+			ensure_resource("file","/usr/local/etc/mail",{
+				ensure => directory
+			})
 
+			package { $packages:
+				provider => $package_provider,
+				package_settings => $package_settings,
+				ensure => installed,
+				require => $package_require
+			} ->
 			file {"/usr/local/etc/mail/mailer.conf":
 				ensure => present,
 				content => template("mailserver/mailer.conf.erb")
 			}
-
 		}
-		default: {
-                        $postfix_pkg = "postfix"
-#                        package {"maildrop":
-#                                ensure => 'installed'
-#                        }
+		'Debian': {
 
-                        package {"postfix":
+                        $packages = "postfix"
+			package {"postfix":
                                 ensure => 'installed'
                         }
                         if $ldap {
@@ -54,8 +53,22 @@ class mailserver::postfix(
                                         ensure => 'installed'
                                 }
                         }
+			if $mysql {
+                                package {'postfix-mysql':
+                                        ensure => 'installed'
+                                }
+			}
+			if $pgsql {
+                                package {'postfix-pgsql':
+                                        ensure => 'installed'
+                                }
+			}
+#                        package {"maildrop":
+#                                ensure => 'installed'
+#                        }
+#
+ 
 		}
 	}
-
 
 }
