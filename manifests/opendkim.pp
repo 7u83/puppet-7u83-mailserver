67c6b8b5981d282a643eb9ceb1e6e0a39bee83cc
7u83 <7u83@mail.ru>
2019-12-05 10:56:09 +0000

    Modified to work with Debian

--- manifests/opendkim.pp
+++ manifests/opendkim.pp
@@ -1,29 +1,29 @@
-# dkim install
 #
+# opendkim installation
 #
 class mailserver::opendkim::params {
         case $::osfamily {
                 'FreeBSD':{
-
 			$service = "milter-opendkim"
 			$cfgdir = "/usr/local/etc/mail"
+			$cfgfile = "/usr/local/etc/mail/opendkim.conf"
 			$keysdir = "/usr/local/etc/mail"
 			$uid='mailnull'
 			$gid='mailnull'
-#			$socket='/var/run/milteropendkim/opendkim.sock'
 			$pkg = "opendkim"
 			$milter_sock='local:/var/run/milteropendkim/opendkim.sock'
+			$pid_file='/var/run/milteropendkim/opendkim.pid'
 		}
-		default: {
+		'Debian': {
 			$service = "opendkim"
 			$cfgdir = "/etc/mail"
-			$keysdir = "/etc/mail"
+			$cfgfile = "/etc/opendkim.conf"
+			$keysdir = "/etc/dkimkeys"
 			$uid='opendkim'
 			$gid='opendkim'
-#			$socket='/var/spool/postfix/private/opendkim'
-			$milter_sock='unix:/var/spool/postfix/private/opendkim'
+			$milter_sock='local:/var/run/opendkim/opendkim.sock'
+			$pid_file='/var/run/opendkim/opendkim.pid'
 			$pkg = "opendkim"
-
 		}
 	}
 }
@@ -38,6 +38,7 @@ class mailserver::opendkim(
 	$uid = $mailserver::opendkim::params::uid,
 	$gid = $mailserver::opendkim::params::gid,
 	$milter_sock = $mailserver::opendkim::params::milter_sock,
+	$pid_file = $mailserver::opendkim::params::pid_file,
 
 ) inherits mailserver::opendkim::params
 {
@@ -48,7 +49,15 @@ class mailserver::opendkim(
 	$dkmynetworks = join($mynetworks,", ")
 
 
-	if $keyfile_content or $keyfile_source {	
+	if $keyfile_content or $keyfile_source {
+
+		file { "$keysdir":
+			ensure => directory,
+			owner => "$uid",
+			group => "$gid",
+			mode => '700',
+		}
+
 		file { "$keysdir/${selector}.private":
 			ensure => present,
 			source => $keyfile_source,
@@ -56,7 +65,11 @@ class mailserver::opendkim(
 			mode => "600",
 			owner => "$uid",
 			group => "$gid",
-			require => Package[$pkg],
+			require => [
+				Package[$pkg], 
+				File["$keysdir"]
+			],
+			notify => Service[$service],
 		}
 	}
 
@@ -78,7 +91,7 @@ class mailserver::opendkim(
 		anchor {"pre-config":}
 	}
 
-	file { "$cfgdir/opendkim.conf":
+	file { "$cfgfile":
 		ensure => present,
 		content => template("mailserver/opendkim.conf.erb"),
 		require => [Package[$pkg],Anchor[pre-config]],
@@ -87,11 +100,8 @@ class mailserver::opendkim(
 
 	service {$service:
 		ensure => running,
-		require => File["$cfgdir/opendkim.conf"],
+		require => File["$cfgfile"],
 	}
 	
 }
-
-
-
 
