9727a4da0bafacb437c7026e024d5b90a1be5e2c
7u83 <7u83@mail.ru>
2019-12-08 22:28:36 +0000

    Added saslauthd and imapd param

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,9 +4,13 @@ class mailserver::params {
         case $::osfamily {
                 'FreeBSD':{
 			$mta = 'sendmail'
+			$saslauthd = 'cyrus::saslauthd'
+			$imapd = 'uwimap'
 		}
 		'Debian': {
 			$mta = 'postfix'
+			$saslauthd = 'cyrus::saslauthd'
+			$imapd = 'uwimapd'
 		}
 	}
 
